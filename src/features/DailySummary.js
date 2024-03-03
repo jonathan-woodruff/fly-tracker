22c5ce867276193c00a4183a473df612ebd6ed97
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2024-03-03 10:51:16 -0600

    add dailysummary feature and start the tracker component

---
+++ src/features/DailySummary.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectBodyTouches, selectFaceTouches, selectKills } from './formsSlice';
+
+export default function DailySummary() {
+  const numBodyTouches = useSelector(selectBodyTouches);
+  const numFaceTouches = useSelector(selectFaceTouches);
+  const numKills = useSelector(selectKills);
+
+  return (
+    <>
+      <h2>Today's Numbers</h2>
+      <p>Body Touches: {numBodyTouches}</p>
+      <p>Face Touches: {numFaceTouches}</p>
+      <p>Kills: {numKills}</p>
+    </>
+  );
+}
